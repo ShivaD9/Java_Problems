f4a98910d22293f6d33d5a94eff1f1c519d0bf7c
ShivaD9 <bhosleshiv143@gmail.com>
2024-07-31 18:55:23 +0530

    Update

--- ReverseString.java
+++ ReverseString.java
@@ -7,14 +7,27 @@ public class ReverseString {
         String str = "ABCD";
         String rev = "";
     
-        // Logic-1 (Concatenation)
-        int len = str.length(); // 4
-        
-        for(int i =len-1 ; i >= 0; i--){ //3 2 1 0
-            rev = rev + str.charAt(i);  // D C B A
-        }
+        // // Logic-1 (Concatenation)
+        // int len = str.length(); // 4
+        // for(int i =len-1 ; i >= 0; i--){ //3 2 1 0
+        //     rev = rev + str.charAt(i);  // D C B A
+        // }
+        // System.out.println("Reverse String "+ rev);
 
-        System.out.println("Reverse String "+ rev);
+
+        // // Logic-2 (Using Character Array)
+        // char a[]=str.toCharArray();
+        // int len=a.length;  // 4
+        // for(int i=len-1;i>=0;i--){  //3 2 1 0
+        //     rev =rev + a[i];   // D C B A
+        // }        
+        // // System.out.println("Reverse String "+ rev);
+
+
+        // Logic-3 (Using StringBuffer class)
+        // StringBuffer sb = new StringBuffer(str);
+        // System.out.println(sb.reverse());
+        // System.out.println("Reverse String "+ rev);
     }
 
 }
